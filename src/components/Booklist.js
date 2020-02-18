e52500544a7a15f0af16c5c1e4f25367fad2c2c9
WingedGuard <katiepgray@gmail.com>
2020-02-18 14:02:14 -0500

    getting started

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component {
+  render() {
+    return (
+      <div className="booklist">
+        <ul>
+          <li>neverwhere</li>
+          <li>good omens</li>
+          <li>men at arms</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Booklist;
