67f13d30953b1dcb08b22ee881fb01572a915934
WingedGuard <katiepgray@gmail.com>
2020-02-19 13:13:05 -0500

    lesson 8 Consuming Multiple Contexts

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Navbar from "./components/Navbar";
 import Booklist from "./components/Booklist";
 import ThemeContextProvider from "./contexts/themeContext";
+import AuthContextProvider from "./contexts/authContext";
 import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <Booklist />
-        <ThemeToggle />
+        <AuthContextProvider>
+
+          <Navbar />
+          <Booklist />
+          <ThemeToggle />
+
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
